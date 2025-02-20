48514d996e50b127adb2b4e9f267147bf0860ea0
jeffshrager <jshrager@stanford.edu>
2025-02-20 10:36:39 -0800

    no message

--- iplv.lisp
+++ iplv.lisp
@@ -2,6 +2,9 @@
 
 ;;; Things not implemented: Aux storage, various J functions, 
 
+;;; FFF Maybe use Lisp lists instead of the morass of symbol table pointers that
+;;; require a mess of renaming.
+
 (declaim (optimize (debug 3) (safety 3) (speed 0) (space 0) (compilation-speed 0)))
 
 (defstruct (card (:print-function print-card)) comments type name sign pq symb link comments.1 id)
@@ -123,19 +126,19 @@
 	    (cond ((string-equal "" (card-type card))
 		   (when (global-symbol? name)
 		     (ipl-trace :load "Loading global name: ~a~%" name)
-		     (save-reversed-cards cards)
+		     (save-cards (reverse cards))
 		     (setf cards nil))
 	      	   (push card cards))
 		  ((and (string-equal "5" (card-type card))
 			(global-symbol? (card-symb card)))
 		   (format t "*** Execution start at ~a ***~%" (card-symb card))
-		   (save-reversed-cards cards)
+		   (save-cards (reverse cards))
 		   (run (card-symb card)))
 		  (t (ipl-trace :load "Ignoring: ~s~%" read-row))))
-	  finally (save-reversed-cards cards)
+	  finally (save-cards (reverse cards))
 	  )))
 
-(defun save-reversed-cards (cards)
+(defun save-cards (cards)
   ;; This does a really ugly hack (or, one might consider it a
   ;; clever hack?) Once we have the thing completely in hand, we
   ;; change the local symbols to FN_9-... and save those as separate
@@ -143,7 +146,6 @@
   ;; through, and also use sub sections of code in J100 meta-calls
   ;; (ugh!)
   (when cards
-    (setf cards (reverse cards))
     (let* ((top-name (card-name (car cards)))
 	   (local-symbols.new-names
 	    (uniquify-list
@@ -155,24 +157,32 @@
       (convert-local-symbols cards local-symbols.new-names)
       (setf (gethash top-name *symtab*) cards)
       (ipl-trace :load "Saved: ~a~%" (card-name (car cards)))
-      (loop for (nil . new-name) in local-symbols.new-names
-	    as subcode = (loop for card+ on cards
-			       when (string-equal (card-name (car card+)) new-name)
-			       do (return card+))
-	    do
-	    (setf (gethash new-name *symtab*) subcode)
-	    (ipl-trace :load "Saved subcode: ~a~%" new-name))
-      (setf cards nil))))
+      (save-subcodes cards))))
+
+;;; WWW %%% This is duplicative! Each sublist contains all the sublists after it.
+;;; it!
+
+(defun save-subcodes (l)
+    (loop for cards on l
+	  as name = (card-name (car cards))
+	  unless (string-equal "" name)
+	  do (setf (gethash name *symtab*) cards)
+	  (ipl-trace :load "Saved subcode: ~a~%" name)))
+
+;;; WWW This is used both on loading, and in list copying.
 
 (defun convert-local-symbols (cards local-symbols.new-names)
   (labels ((replace-symbols (card accname.accessor)
 	     (let ((new-name (cdr (assoc (funcall (cdr accname.accessor) card) local-symbols.new-names :test #'string-equal))))
-	       (when new-name (fucking-setf (car accname.accessor) card new-name)))))
+	       (when new-name (setf* (car accname.accessor) card new-name)))))
     (loop for card in cards
 	  do (loop for accname.accessor in *symbol-col-accessors*
 		   do (replace-symbols card accname.accessor)))))
 			    
-(defun fucking-setf (accname card new-name)
+;;; This stupidity is needed because setf doesn't know how to set a value based
+;;; on an arbitrary accessor.
+
+(defun setf* (accname card new-name)
   (case accname
     (card-name (setf (card-name card) new-name))
     (card-symb (setf (card-symb card) new-name))
@@ -220,7 +230,6 @@
 ("J10" . 18) 
 ("J136" . 17) 
 ("J155" . 17) 
-("J120" . 17) 
 ("J72" . 16) 
 ("J154" . 16) 
 ("J5" . 15) 
@@ -297,64 +306,65 @@
 
 (defun setup-j-fns ()
 
-  (defj J6 (format t "WWW J6 IS UNIMPLEMENTED !!!~%"))
-  (defj J4 (format t "WWW J4 IS UNIMPLEMENTED !!!~%"))
-  (defj J81 (format t "WWW J81 IS UNIMPLEMENTED !!!~%"))
-  (defj J60 (format t "WWW J60 IS UNIMPLEMENTED !!!~%"))
-  (defj J71 (format t "WWW J71 IS UNIMPLEMENTED !!!~%"))
-  (defj J10 (format t "WWW J10 IS UNIMPLEMENTED !!!~%"))
-  (defj J136 (format t "WWW J136 IS UNIMPLEMENTED !!!~%"))
-  (defj J155 (format t "WWW J155 IS UNIMPLEMENTED !!!~%"))
-  (defj J120 (format t "WWW J120 IS UNIMPLEMENTED !!!~%"))
-  (defj J72 (format t "WWW J72 IS UNIMPLEMENTED !!!~%"))
-  (defj J5 (format t "WWW J5 IS UNIMPLEMENTED !!!~%"))
-  (defj J2 (format t "WWW J2 IS UNIMPLEMENTED !!!~%"))
-  (defj J11 (format t "WWW J11 IS UNIMPLEMENTED !!!~%"))
-  (defj J31 (format t "WWW J31 IS UNIMPLEMENTED !!!~%"))
-  (defj J38 (format t "WWW J38 IS UNIMPLEMENTED !!!~%"))
-  (defj J161 (format t "WWW J161 IS UNIMPLEMENTED !!!~%"))
-  (defj J50 (format t "WWW J50 IS UNIMPLEMENTED !!!~%"))
-  (defj J90 (format t "WWW J90 IS UNIMPLEMENTED !!!~%"))
-  (defj J33 (format t "WWW J33 IS UNIMPLEMENTED !!!~%"))
-  (defj J160 (format t "WWW J160 IS UNIMPLEMENTED !!!~%"))
-  (defj J9 (format t "WWW J9 IS UNIMPLEMENTED !!!~%"))
-  (defj J82 (format t "WWW J82 IS UNIMPLEMENTED !!!~%"))
-  (defj J157 (format t "WWW J157 IS UNIMPLEMENTED !!!~%"))
-  (defj J64 (format t "WWW J64 IS UNIMPLEMENTED !!!~%"))
-  (defj J34 (format t "WWW J34 IS UNIMPLEMENTED !!!~%"))
-  (defj J74 (format t "WWW J74 IS UNIMPLEMENTED !!!~%"))
-  (defj J8 (format t "WWW J8 IS UNIMPLEMENTED !!!~%"))
-  (defj J116 (format t "WWW J116 IS UNIMPLEMENTED !!!~%"))
-  (defj J7 (format t "WWW J7 IS UNIMPLEMENTED !!!~%"))
-  (defj J14 (format t "WWW J14 IS UNIMPLEMENTED !!!~%"))
-  (defj J133 (format t "WWW J133 IS UNIMPLEMENTED !!!~%"))
-  (defj J18 (format t "WWW J18 IS UNIMPLEMENTED !!!~%"))
-  (defj J68 (format t "WWW J68 IS UNIMPLEMENTED !!!~%"))
-  (defj J41 (format t "WWW J41 IS UNIMPLEMENTED !!!~%"))
-  (defj J125 (format t "WWW J125 IS UNIMPLEMENTED !!!~%"))
-  (defj J124 (format t "WWW J124 IS UNIMPLEMENTED !!!~%"))
-  (defj J3 (format t "WWW J3 IS UNIMPLEMENTED !!!~%"))
-  (defj J51 (format t "WWW J51 IS UNIMPLEMENTED !!!~%"))
-  (defj J91 (format t "WWW J91 IS UNIMPLEMENTED !!!~%"))
-  (defj J43 (format t "WWW J43 IS UNIMPLEMENTED !!!~%"))
-  (defj J17 (format t "WWW J17 IS UNIMPLEMENTED !!!~%"))
-  (defj J19 (format t "WWW J19 IS UNIMPLEMENTED !!!~%"))
-  (defj J42 (format t "WWW J42 IS UNIMPLEMENTED !!!~%"))
-  (defj J32 (format t "WWW J32 IS UNIMPLEMENTED !!!~%"))
-  (defj J65 (format t "WWW J65 IS UNIMPLEMENTED !!!~%"))
-  (defj J75 (format t "WWW J75 IS UNIMPLEMENTED !!!~%"))
-  (defj J78 (format t "WWW J78 IS UNIMPLEMENTED !!!~%"))
-  (defj J35 (format t "WWW J35 IS UNIMPLEMENTED !!!~%"))
-  (defj J36 (format t "WWW J36 IS UNIMPLEMENTED !!!~%"))
-  (defj J184 (format t "WWW J184 IS UNIMPLEMENTED !!!~%"))
-  (defj J111 (format t "WWW J111 IS UNIMPLEMENTED !!!~%"))
-  (defj J138 (format t "WWW J138 IS UNIMPLEMENTED !!!~%"))
-  (defj J137 (format t "WWW J137 IS UNIMPLEMENTED !!!~%"))
-  (defj J66 (format t "WWW J66 IS UNIMPLEMENTED !!!~%"))
-  (defj J115 (format t "WWW J115 IS UNIMPLEMENTED !!!~%"))
-  (defj J183 (format t "WWW J115 IS UNIMPLEMENTED !!!~%"))
-  (defj J181 (format t "WWW J115 IS UNIMPLEMENTED !!!~%"))
-  (defj J130 (format t "WWW J115 IS UNIMPLEMENTED !!!~%"))
+  (defj J2 (ipl-trace :jfns "WWW J2 IS UNIMPLEMENTED !!!~%"))
+  (defj J3 (ipl-trace :jfns "WWW J3 IS UNIMPLEMENTED !!!~%"))
+  (defj J4 (ipl-trace :jfns "WWW J4 IS UNIMPLEMENTED !!!~%"))
+  (defj J5 (ipl-trace :jfns "WWW J5 IS UNIMPLEMENTED !!!~%"))
+  (defj J6 (ipl-trace :jfns "WWW J6 IS UNIMPLEMENTED !!!~%"))
+  (defj J7 (ipl-trace :jfns "WWW J7 IS UNIMPLEMENTED !!!~%"))
+  (defj J8 (ipl-trace :jfns "WWW J8 IS UNIMPLEMENTED !!!~%"))
+  (defj J9 (ipl-trace :jfns "WWW J9 IS UNIMPLEMENTED !!!~%"))
+  (defj J10 (ipl-trace :jfns "WWW J10 IS UNIMPLEMENTED !!!~%"))
+  (defj J11 (ipl-trace :jfns "WWW J11 IS UNIMPLEMENTED !!!~%"))
+  (defj J14 (ipl-trace :jfns "WWW J14 IS UNIMPLEMENTED !!!~%"))
+  (defj J17 (ipl-trace :jfns "WWW J17 IS UNIMPLEMENTED !!!~%"))
+  (defj J18 (ipl-trace :jfns "WWW J18 IS UNIMPLEMENTED !!!~%"))
+  (defj J19 (ipl-trace :jfns "WWW J19 IS UNIMPLEMENTED !!!~%"))
+  (defj J31 (ipl-trace :jfns "WWW J31 IS UNIMPLEMENTED !!!~%"))
+  (defj J32 (ipl-trace :jfns "WWW J32 IS UNIMPLEMENTED !!!~%"))
+  (defj J33 (ipl-trace :jfns "WWW J33 IS UNIMPLEMENTED !!!~%"))
+  (defj J34 (ipl-trace :jfns "WWW J34 IS UNIMPLEMENTED !!!~%"))
+  (defj J35 (ipl-trace :jfns "WWW J35 IS UNIMPLEMENTED !!!~%"))
+  (defj J36 (ipl-trace :jfns "WWW J36 IS UNIMPLEMENTED !!!~%"))
+  (defj J38 (ipl-trace :jfns "WWW J38 IS UNIMPLEMENTED !!!~%"))
+  (defj J41 (ipl-trace :jfns "WWW J41 IS UNIMPLEMENTED !!!~%"))
+  (defj J42 (ipl-trace :jfns "WWW J42 IS UNIMPLEMENTED !!!~%"))
+  (defj J43 (ipl-trace :jfns "WWW J43 IS UNIMPLEMENTED !!!~%"))
+  (defj J50 (ipl-trace :jfns "WWW J50 IS UNIMPLEMENTED !!!~%"))
+  (defj J51 (ipl-trace :jfns "WWW J51 IS UNIMPLEMENTED !!!~%"))
+  (defj J60 (ipl-trace :jfns "WWW J60 IS UNIMPLEMENTED !!!~%"))
+  (defj J64 (ipl-trace :jfns "WWW J64 IS UNIMPLEMENTED !!!~%"))
+  (defj J65 (ipl-trace :jfns "WWW J65 IS UNIMPLEMENTED !!!~%"))
+  (defj J66 (ipl-trace :jfns "WWW J66 IS UNIMPLEMENTED !!!~%"))
+  (defj J68 (ipl-trace :jfns "WWW J68 IS UNIMPLEMENTED !!!~%"))
+  (defj J71 (ipl-trace :jfns "WWW J71 IS UNIMPLEMENTED !!!~%"))
+  (defj J72 (ipl-trace :jfns "WWW J72 IS UNIMPLEMENTED !!!~%"))
+  (defj J74 (ipl-trace :jfns "WWW J74 IS UNIMPLEMENTED !!!~%"))
+  (defj J75 (ipl-trace :jfns "WWW J75 IS UNIMPLEMENTED !!!~%"))
+  (defj J76 (ipl-trace :jfns "WWW J76 IS UNIMPLEMENTED !!!~%"))
+  (defj J78 (ipl-trace :jfns "WWW J78 IS UNIMPLEMENTED !!!~%"))
+  (defj J81 (ipl-trace :jfns "WWW J81 IS UNIMPLEMENTED !!!~%"))
+  (defj J82 (ipl-trace :jfns "WWW J82 IS UNIMPLEMENTED !!!~%"))
+  (defj J90 (ipl-trace :jfns "WWW J90 IS UNIMPLEMENTED !!!~%"))
+  (defj J91 (ipl-trace :jfns "WWW J91 IS UNIMPLEMENTED !!!~%"))
+  (defj J111 (ipl-trace :jfns "WWW J111 IS UNIMPLEMENTED !!!~%"))
+  (defj J115 (ipl-trace :jfns "WWW J115 IS UNIMPLEMENTED !!!~%"))
+  (defj J116 (ipl-trace :jfns "WWW J116 IS UNIMPLEMENTED !!!~%"))
+  (defj J124 (ipl-trace :jfns "WWW J124 IS UNIMPLEMENTED !!!~%"))
+  (defj J125 (ipl-trace :jfns "WWW J125 IS UNIMPLEMENTED !!!~%"))
+  (defj J130 (ipl-trace :jfns "WWW J130 IS UNIMPLEMENTED !!!~%"))
+  (defj J133 (ipl-trace :jfns "WWW J133 IS UNIMPLEMENTED !!!~%"))
+  (defj J136 (ipl-trace :jfns "WWW J136 IS UNIMPLEMENTED !!!~%"))
+  (defj J137 (ipl-trace :jfns "WWW J137 IS UNIMPLEMENTED !!!~%"))
+  (defj J138 (ipl-trace :jfns "WWW J138 IS UNIMPLEMENTED !!!~%"))
+  (defj J155 (ipl-trace :jfns "WWW J155 IS UNIMPLEMENTED !!!~%"))
+  (defj J157 (ipl-trace :jfns "WWW J157 IS UNIMPLEMENTED !!!~%"))
+  (defj J160 (ipl-trace :jfns "WWW J160 IS UNIMPLEMENTED !!!~%"))
+  (defj J161 (ipl-trace :jfns "WWW J161 IS UNIMPLEMENTED !!!~%"))
+  (defj J176 (ipl-trace :jfns "WWW J176 IS UNIMPLEMENTED !!!~%"))
+  (defj J181 (ipl-trace :jfns "WWW J181 IS UNIMPLEMENTED !!!~%"))
+  (defj J183 (ipl-trace :jfns "WWW J183 IS UNIMPLEMENTED !!!~%"))
+  (defj J184 (ipl-trace :jfns "WWW J184 IS UNIMPLEMENTED !!!~%"))
 
   (defj J73 ;; Copy list
       (setf (h0)
@@ -362,17 +372,6 @@
 	     (if (stringp arg0) (*val+ arg0)
 		 (if (listp arg0) arg0
 		     (error "J73 got ARG0=~s" arg0))))))
-
-  (defj J76 
-      ;; INSERT LIST (O) AFTER CELL (1) AND LOCATE LAST SYMBOL. List (0) is
-      ;; assume to be describable. Its head is erased (if local, the symbol in
-      ;; the head is erased as a list structure). The string of list cells is
-      ;; inserted after cell (1): LINK of cell (1) is the name of the first list
-      ;; cell, and LINK of the last cell of the string is the name of the cell
-      ;; originally occurring after cell (1). The output (O) is the name of the
-      ;; last cell in the inserted string and H5 is set+. If list (0) has no
-      ;; list cells, then the output (0) is the input (1) and H5 is set -.
-      (format t "WWW J76 (INSERT LIST (O) AFTER CELL (1) AND LOCATE LAST SYMBOL) is UNIMPLEMENTED !!!~%"))
 
   (defj J100
       ;; J100 GENERATE SYMBOLS FROM LIST (1) FOR SUBPROCESS (0). The subprocess
@@ -387,6 +386,16 @@
 	    (ipl-eval arg0)
 	    (pop (h0+))
 	    ))
+
+  (defj J120
+      ;; COPY (0). The output (0) names a new cell containing the identical
+      ;; contents to (0). The name is local if the input (0) is local; other-
+      ;; wise, it is internal.
+      (let ((l (if (stringp (h0)) (*val+ (h0)) (h0)))
+	    (new-name (format nil "~a" (gensym "+"))))
+	(ipl-trace :jfns "J120 created new list pointer ~s from ~s~%" new-name l)
+	(setf (*val+ new-name) l)
+	(setf (h0) new-name)))
 
   (defj J147 ;; Mark routine to trace
       (format t "WWW J147 (Mark routine to trace) is UNIMPLEMENTED !!!~%"))
@@ -411,6 +420,39 @@
 	      (t (setf (h5) "-")))))
   )
 
+;;; Copying an IPL list is a tricky because they aren't represented like normal
+;;; lisp lists (maybe they shold be?) but instead are a pile of cards where
+;;; internal structure results from the symb and links pointing to other named
+;;; cards all at the top level. In order to do this we need to go through and
+;;; make a local symbol table, and then change all the symbols as we go. (If we
+;;; knew that the list was guaranteed to be forward-pointing only, we could do
+;;; this in one pass.) Importantly, not all symbols in a list point to the list
+;;; itself, so we don't want to be changing those that don't! WWW We also have
+;;; to chase every list pointed to by this one! Ugh!
+
+(defun J74-copy-ipl-list**WRONG! (l &aux local-symbols.new-names)
+  ;; *** THIS IS BROKEN -- IT NEEDS TO COPY THE ENTIRE LIST STRUCTURE ***
+  ;; First pass make the symbol table of the names of symbols in the list
+  ;; itself, and give them new names. These are the only ones we want to
+  ;; change.
+  (ipl-trace :run-full "Copying list: ~s~%" l)
+  (loop for card in l
+	as name = (card-name card)
+	when (not (string-equal "" name))
+	do (push (cons name (format nil "~a" (gensym "+")))
+		 local-symbols.new-names))
+  ;; Okay, so now all we need to do is copy the list an replace the names in
+  ;; the cards with those in the symtab
+  (let ((new-list
+	 (loop for card in l
+	       collect (convert-local-symbols cards local-symbols.new-names))))
+    ;; Finally, register all the new partial lists in the symbtab
+    (save-list-and-sublists l)
+    ;; And return the new-list in (0)
+    (ipl-trace :run-full "   ... New list: ~s~%" new-list)
+    (setf (h0) new-list)
+    ))
+	
 ;;; ===================================================================
 ;;; This is the core of the emulator. It directly implements "3.15 THE
 ;;; INTERPRETATION CYCLE", pg. 164 of the IPL-V manual. This is actually kinda
@@ -583,5 +625,5 @@
 
 (untrace)
 (trace ipl-eval)
-(setf *ipl-trace-list* '(:cards :io)) ;; :load :run :jfns :run-full :cards :io
+(setf *ipl-trace-list* '(:jfns :cards :io)) ;; :load :run :jfns :run-full :cards :io
 (load-ipl "runs/20250214/LT.lisp")
