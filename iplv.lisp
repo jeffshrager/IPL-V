cc2c55d0971ab825e3ca55230f6be3092197cc6a
jeffshrager <jshrager@stanford.edu>
2026-01-30 21:05:46 -0800

    Still don't get 10W1, but found another minor problem.

--- iplv.lisp
+++ iplv.lisp
@@ -1588,10 +1588,12 @@
 ;;; symbol in head; and sets H5- if (0) is a termination symbol.
 
   (defj J81 ([0]) "FIND THE 1st (non-head) SYMBOL OF (0)" 
-	(j8n-helper (cell-link (<== [0])) 1))
+	(j8n-helper (cell-link (<== [0])) 1)
+	(poph0 1))
 
   (defj J82 ([0]) "FIND THE 2nd (non-head0 SYMBOL OF (0)"
-	(j8n-helper (cell-link (<== [0])) 2))
+	(j8n-helper (cell-link (<== [0])) 2)
+	(poph0 1))
 	      
   ;; J9n CREATE A LIST OF THE n SYMBOLS (n-1), (n-2), ..., (1), (0), 0
   ;; < n < 9. The order is (n-1) first, (n-2) second, ..., (0)
@@ -2932,8 +2934,8 @@ restarts (invokable by number or by possibly-abbreviated name):
 (progn ;; LT 
   (set-trace-mode :none)
   (setf *j15-mode* :clear-dl) ;; Documentation ambiguity, alt: :clear-dl :delete-dl
-  ;;(setf *!!* '(:run :jcalls :jdeep) *cell-tracing-on* t) ;; :run :jcalls :jdeep :alerts :s :dr-memory :gentrace
-  ;;(setf *trace-cell-names-or-exprs* '("H0" "W0" "W1" "W2") *cell-tracing-on* t)
+  (setf *!!* '(:run :jcalls :jdeep) *cell-tracing-on* t) ;; :run :jcalls :jdeep :alerts :s :dr-memory :gentrace
+  (setf *trace-cell-names-or-exprs* '("H0" "W0" "W1" "W2") *cell-tracing-on* t)
   ;; ************ NOTE P055R000 L11 HACK THAT MUST STAY IN PLACE! ************
   ;; (It's been over-riden by LTFixed code.)
   ;;(trace j8n-helper ipush) 
